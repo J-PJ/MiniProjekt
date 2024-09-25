fe962605288c926fdc09f3e0e8b3d9c1d5d5dfe1
Jens Jørgensen <147208366+J-PJ@users.noreply.github.com>
2024-09-25 09:45:43 +0200

    median filter

--- Main.py
+++ Main.py
@@ -9,13 +9,15 @@ image = cv.imread("4.jpg", cv.IMREAD_COLOR)  # type: ignore
 
 # Convert BGR to HSV
 hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-maskhav = cv.inRange(hsv, (100, 90, 90), (120, 255, 255))#blue
-maskmark = cv.inRange(hsv, (0, 230, 80), (35, 255, 255))#gul
-maskskov = cv.inRange(hsv, (0, 80, 0), (50, 255, 60))#grøn
+maskhav = cv.inRange(hsv, (95, 180, 90), (122, 255, 255))#blue
+maskmark = cv.inRange(hsv, (25, 150, 80), (35, 255, 255))#gul
+maskskov = cv.inRange(hsv, (40, 50, 0), (65, 255, 60))#grøn
 
+maskmark_s = cv.medianBlur(maskmark, ksize = 3)
+maskskov_s = cv.medianBlur(maskskov, ksize = 3)
 
 cv.imshow("Hav", maskhav)  # type: ignore
-cv.imshow("Mark", maskmark)  # type: ignore
-cv.imshow("Skov", maskskov)  # type: ignore
+cv.imshow("Mark", maskmark_s)  # type: ignore
+cv.imshow("Skov", maskskov_s)  # type: ignore
 cv.waitKey(0)  # type: ignore
 cv.destroyAllWindows()  # Close the window after a key press
